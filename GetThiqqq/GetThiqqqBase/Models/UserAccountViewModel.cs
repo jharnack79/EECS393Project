be57f8e84e78b8f33d0a36f0c57368fbc1a191c9
Jason Harnack <jfh79@case.edu>
2018-02-21 23:36:34 -0500

    Adding views and web app portion

---
+++ GetThiqqq/GetThiqqqBase/Models/UserAccountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GetThiqqqBase.Models
+{
+    public class UserAccountViewModel
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string UserPassword { get; set; }
+
+        public string UserEmail { get; set; }
+
+    }
+}
---
+++ GetThiqqq/GetThiqqqBase/Models/UserProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetThiqqqBase.Models
+{
+    public class UserProfileViewModel
+    {
+        public int Id { get; set; }
+
+        public int UserAccountId { get; set; }
+
+        public string Address { get; set; }
+    }
+}
