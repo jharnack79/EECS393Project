118aa08ecac574759e975902fc0b5cdd84b00643
Jason Harnack <jfh79@case.edu>
2018-02-22 20:07:05 -0500

    Adding initial user account methods and structure, also adding zip file for AWS Site, not working atm, but will tackle later

---
+++ GetThiqqq/GetThiqqq/UserAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetThiqqq
+{
+    public interface IUserAccount
+    {
+        UserAccount CreateAccount(string userName, string Password, string EmailAddress);
+
+        bool IsUsernameTaken(string userName);
+
+        bool IsPasswordSecure(string password);
+
+        bool IsEmailAddressRegistered(string emailAddress);
+    }
+
+    public class UserAccount : IUserAccount
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public UserAccount CreateAccount(string userName, string password, string emailAddress)
+        {
+            //Add information to DB
+            //Get User account ID and post that to each page
+            //Send verification email
+            return null;
+        }
+
+        public bool IsUsernameTaken(string userName)
+        {
+            return false;
+        }
+
+        public bool IsPasswordSecure(string password)
+        {
+            return false;
+        }
+
+        public bool IsEmailAddressRegistered(string emailAddress)
+        {
+            return false; 
+        }
+    }
+}
--- GetThiqqq/GetThiqqqBase/Controllers/AccountController.cs
+++ GetThiqqq/GetThiqqqBase/Controllers/AccountController.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using GetThiqqq;
+using GetThiqqqBase.Models;
 
 namespace GetThiqqqBase.Controllers
 {
     public class AccountController : Controller
     {
+        public IUserAccount _userAccount;
+
+        public AccountController(IUserAccount userAccount)
+        {
+            _userAccount = userAccount;
+        }
+
         public IActionResult Login()
         {
+            return View();
+        }
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        public IActionResult CreateAccount(UserAccountViewModel userAccountViewModel)
+        {
+
+            if (_userAccount.IsEmailAddressRegistered(userAccountViewModel.UserEmail) && _userAccount.IsPasswordSecure(userAccountViewModel.UserPassword))
+            {
+                RedirectToAction("CreateAccount");
+            }
+
             return View();
         }
     }
