b8527cb2eaa830da12e21a685db04bce6199915f
Jason Harnack <jfh79@case.edu>
2018-03-27 19:05:33 -0400

    Missed a few files

---
+++ GetThiqqq/GetThiqqqBase/Views/Account/SubmitAccount.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GetThiqqqBase.Views.Account
+{
+    public class SubmitAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
