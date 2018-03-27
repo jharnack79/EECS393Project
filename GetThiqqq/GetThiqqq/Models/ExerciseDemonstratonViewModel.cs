191ee8c8d6fba03d4df2894431fd734a22fdf29a
Jason Harnack <jfh79@case.edu>
2018-03-27 19:17:46 -0400

    Adding demnstration views and viewModel

---
+++ GetThiqqq/GetThiqqq/Models/ExerciseDemonstratonViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GetThiqqq.Services;
+
+namespace GetThiqqq.Models
+{
+    public class ExerciseDemonstratonViewModel
+    {
+        public int UserAccountId { get; set; }
+
+        public Exercise Exercise { get; set; }
+    }
+}
