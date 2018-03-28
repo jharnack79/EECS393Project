20d13964749b5b5120bd0dd6ec46b41a755d7f08
Jason Harnack <jfh79@case.edu>
2018-03-27 21:47:57 -0400

    Working on ability to execute insert commands for sql

---
+++ GetThiqqq/GetThiqqq/Constants/DatabaseConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetThiqqq.Constants
+{
+    public class DatabaseConstants
+    {
+        public const string ConnectionString = "TBD";
+    }
+}
---
+++ GetThiqqq/GetThiqqq/Repository/UserAccountRepository.cs
@@ -0,0 +1,32 @@
+﻿using GetThiqqq.Models;
+using GetThiqqq.Constants;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GetThiqqq.Repository
+{
+    public interface IUserAccountRepository
+    {
+        void AddNewAccount(CreateAccountViewModel createAccountViewModel);
+    }
+
+    public class UserAccountRepository : IUserAccountRepository
+    {
+        public void AddNewAccount(CreateAccountViewModel createAccountViewModel)
+        {
+            var sqlConnection = new SqlConnection(DatabaseConstants.ConnectionString);
+            var cmd = new SqlCommand();
+
+            sqlConnection.Open();
+            cmd.CommandText = " Select Count(*) from UserAccount";
+            cmd.Connection = sqlConnection;
+
+            int newId = (int)cmd.ExecuteScalar() + 1;
+
+            cmd.CommandText = "INSERT INTO UserAccount (Id, Username, Password, EmailAddress) " +
+                "Values("  + newId + ", '" + createAccountViewModel.Username + "', '" + createAccountViewModel.Password +
+                "', '" + createAccountViewModel.EmailAddress + "'";
+
+        }
+    }
+}
