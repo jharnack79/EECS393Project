3e6b4dc9383aea8c2cf750e55cf59f7d5b46cc7f
Jason Harnack <jfh79@case.edu>
2018-04-30 21:49:10 -0400

    Finalizing Startup file issues

--- GetThiqqq/GetThiqqqBase/Startup.cs
+++ GetThiqqq/GetThiqqqBase/Startup.cs
@@ -28,6 +28,7 @@ namespace GetThiqqqBase
             services.AddScoped<IUserAccountRepository, UserAccountRepository>();
             services.AddScoped<IExerciseRepository, ExerciseRepository>();
             services.AddScoped(typeof(UserAccountRepository));
+            services.AddScoped(typeof(ForumPostRepository));
             services.AddScoped<IUserAccountRepository, UserAccountRepository>();
             services.AddScoped<IForumPostRepository, ForumPostRepository>();
             services.AddScoped<IForumTopicRepository, ForumTopicRepository>();
